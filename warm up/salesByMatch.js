d8f8656f85bfd9e596b1b6c29b230254276fef83
lixuling <antonisaputra049@gmail.com>
2022-10-13 08:38:42 +0700

    sales by match

---
+++ warm up/salesByMatch.js
@@ -0,0 +1,19 @@
+const arr = [1,2,1,2,3,4]
+
+function match() {
+  let pair = 0
+  let socks = {}
+  arr.forEach((item) => {
+    if (!socks[item]) {
+      socks[item] = 0
+    }
+   socks[item] = socks[item] + 1
+
+    if (!(socks[item] % 2)) {
+      pair = pair + 1
+    }
+  })
+  return pair
+}
+
+console.log(match());
